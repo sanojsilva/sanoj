bb0770ca16c093c30fd542e9cc74753515cd8292
Sanoj Silva <silvasanoj@gmail.com>
2022-02-11 21:54:01 +0530

    Add a description about me

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+console.clear();
 console.log(`
 Sanoj Silva
+
+I'm a Tech Enthusiast who always look forward to take on new challenges, learn new things and grow with all possible opportunities. I love working with Web technologies. Over the years I have developed many projects from Landing pages, E-commerce Applications to Information Systems.
 `);
