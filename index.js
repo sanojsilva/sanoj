7e4c900250bba23220c259bc66eea571dbb45cc3
Sanoj Silva <silvasanoj@gmail.com>
2022-02-26 09:45:31 +0530

    Remove chalk animation for temporary

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 #!/usr/bin/env node
 
-const ca = require("chalk-animation");
 const myEducation = require("./utils/education");
 const myProjects = require("./utils/projects");
 const intro = require("./utils/intro");
 const header = require("./utils/header");
 const social = require("./utils/social");
-
-const sleep = () => new Promise((res) => setTimeout(res, 2000));
-
-const animation = async () => {
-  const position = ca.rainbow("A FULL STACK DEVELOPER");
-  position.start();
-  await sleep();
-};
 
 console.clear();
 
